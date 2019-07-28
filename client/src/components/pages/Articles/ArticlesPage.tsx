e150c714563e272fe51dfa836f369abac2f3c563
Jodest <jodestic@gmail.com>
2019-07-28 23:21:49 +0300

    types articles

--- client/src/components/pages/Articles/ArticlesPage.tsx
+++ client/src/components/pages/Articles/ArticlesPage.tsx
@@ -1,75 +1,36 @@
 import React from 'react';
-import { compose, bindActionCreators, Dispatch } from 'redux';
-import { connect } from 'react-redux';
+import { compose } from 'redux';
 
-import { fetchArticles } from '../../../store/articles';
-import ArticlesService from '../../../services/ArticlesService';
 import Article from '../Article';
-import Spinner from '../../Spinner';
 import WithArticlesState from './container';
 
 
 export interface Props {
-  fetchArticles: any;
-  updateArticles: any;
   articles: any;
-  loading: any;
-  error: any;
 };
 
-class ArticlesPage extends React.Component<Props> {
-
-  componentDidMount() {
-    console.log(this.props);
-    this.props.fetchArticles();
-  }
-
-  render() {
-    const { articles, loading, error } = this.props;
-    console.log(this.props);
-    if (loading) {
-      return <Spinner />
-    }
-
-    if (error) {
-      return <div>Error</div>;
-    }
-
-    return (
-      <div>
-        <ul className="article-list">
-          {
-            articles.map(({title, author, image, date, text}: any, ind: number) => (
-              <li key={ind}>
-                <Article
-                  title={title}
-                  author={author}
-                  image={image}
-                  date={date}
-                  text={text}
-                />
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = ({ updateArticles: { articles, loading, error } }: any) => ({
-  articles, loading, error
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators({
-    fetchArticles: fetchArticles(new ArticlesService()),
-  }, dispatch);
-};
+const ArticlesPage: React.SFC<Props> = ({ articles }) => (
+  <div>
+    <ul className="article-list">
+      {
+        articles.map(({title, author, image, date, text}: any, ind: number) => (
+          <li key={ind}>
+            <Article
+              title={title}
+              author={author}
+              image={image}
+              date={date}
+              text={text}
+            />
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+);
 
 export default compose(
   //!
-  // WithArticlesState,
-  connect(mapStateToProps, mapDispatchToProps),
+  WithArticlesState,
   // WithArticlesState,
 )(ArticlesPage);
--- client/src/components/pages/Articles/container/WithArticlesState.tsx
+++ client/src/components/pages/Articles/container/WithArticlesState.tsx
@@ -1,31 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { compose, bindActionCreators, Dispatch } from 'redux';
 
 import { fetchArticles } from '../../../../store/articles';
 import ArticlesService from '../../../../services/ArticlesService';
+import Spinner from '../../../Spinner';
 
 export interface Props {
   articles: any;
+  loading: any;
+  error: any;
   fetchArticles: any;
 };
 
-const WithArticlesState = (Wrapped: React.SFC<Props>) => (props: any): any => {
-  if (props.loading) {
-    return <p>Загрузка</p>;
-  }
+export interface Props2 {
+  articles: any;
+};
 
-  if (props.error) {
-    return <p>Ошибка</p>;
-  }
+const WithArticlesState = (Wrapped: React.ComponentType<Props2>) => {
+  return class extends Component<Props & Props2, {}> {
+    componentDidMount() {
+      console.log(this.props);
+      this.props.fetchArticles();
+    }
 
-  return (
-    <Wrapped {...props} />
-  );
+    render() {
+      console.log(this.props);
+      if (this.props.loading) {
+        return <Spinner />;
+      }
+
+      if (this.props.error) {
+        return <p>Error</p>;
+      }
+
+      return (
+        <Wrapped articles={this.props.articles} {...this.props} />
+      );
+    }
+  }
 }
 
-const mapStateToProps = ({ updateArticles }: any) => ({
-  updateArticles
+// const WithArticlesState = (Wrapped: React.SFC<Props>) => ({ articles, error, loading, ...props}: any): any => {
+//   console.log(props);
+
+
+
+//   if (loading) {
+//     return <Spinner />;
+//   }
+
+//   if (error) {
+//     return <p>Error</p>;
+//   }
+
+//   return (
+//     <Wrapped articles={articles} {...props} />
+//   );
+// }
+
+const mapStateToProps = ({ updateArticles: { articles, loading, error } }: any) => ({
+  articles, loading, error
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
