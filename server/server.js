0f90bdb47417017cfec584f662a1a69c001c5f24
Jodest <jodestic@gmail.com>
2019-08-14 22:07:18 +0300

    connect to mongoDB

--- server/server.js
+++ server/server.js
@@ -1,24 +1,60 @@
 const path = require('path');
 const express = require('express');
-const db = require('./config/db');
 const app = express();
-const articlesRoutes = require('./app/routes/articles_routes');
-
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const db = require('./config/db');
 const mongoose = require('mongoose');
+// const articlesRoutes = require('./app/routes/articles_routes');
+const Articles = require('./models/article');
+const articlesRoutes = express.Router();
 
 const PORT = process.env.PORT | 3000;
 
-console.log(path.join(__dirname, '../client/src'));
+// console.log(path.join(__dirname, '../client/src'));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.use('/api/articles', articlesRoutes);
+// app.use('/api/articles', articlesRoutes);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://dmitry:hHRZNSA8QneflQeo@cluster0-fq59z.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', function() {
+  console.log("MongoDB database connection established successfully");
+})
 
 
+// app.use((req, res, next) => {
+//   res.sendFile('/index.html');
+// });
 
-app.use((req, res, next) => {
-  res.sendFile('/index.html');
+//mongodb+srv://dmitry:<password>@cluster0-fq59z.mongodb.net/test?retryWrites=true&w=majority
+
+//dmitry
+//hHRZNSA8QneflQeo
+
+articlesRoutes.route('/').get(function(req, res) {
+  Articles.find(function(err, articles) {
+      if (err) {
+          console.log(err);
+      } else {
+          res.json(articles);
+      }
+  });
 });
+
+articlesRoutes.route('/:id').get(function(req, res) {
+  let id = req.params.id;
+  Articles.findById(id, function(err, articles) {
+      res.json(articles);
+  });
+});
+
+app.use('/articles', articlesRoutes);
 
 app.listen(PORT, () => {
   console.log('We are live on ' + PORT);
@@ -26,7 +62,7 @@ app.listen(PORT, () => {
 
 // async function start() {
 //   try {
-//     await mongoose.connect(db.url, { useNewUrlParser: true })
+//     await mongoose.connect('mongodb+srv://dmitry:hHRZNSA8QneflQeo@cluster0-fq59z.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
 //     app.listen(PORT, () => {
 //       console.log('We are live on ' + PORT);
 //     });
